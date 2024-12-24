c23473fec49a7f3bd845a0c3a36bde30988d0d78
godless-developer <bbtulga98@gmail.com>
2024-12-24 17:48:36 +0800

    home

---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <div className="search">
+            <div>
+                <input type="text" placeholder="116,293"/>
+            </div>
+            <div></div>
+        </div>
+    )
+}
